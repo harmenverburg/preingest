75a53be3fbb0103e2e740827f7c5ae25b806107c
nhapreingest <73949448+nhapreingest@users.noreply.github.com>
2021-01-11 15:45:46 +0100

    Minor typo fix.

--- preingest/Noord.HollandsArchief.Pre.Ingest.WebApi/Controllers/PreingestController.cs
+++ preingest/Noord.HollandsArchief.Pre.Ingest.WebApi/Controllers/PreingestController.cs
@@ -719,7 +719,7 @@ namespace Noord.HollandsArchief.Pre.Ingest.WebApi.Controllers
             return new JsonResult(new { Message = String.Format("Sip creator is started."), SessionId = guid, ActionId = processId });
         }
 
-        [HttpPost("excelcreator/{guid}", Name = "Generate Excel report", Order = 14)]
+        [HttpPost("excelcreator/{guid}", Name = "Generate Excel report", Order = 15)]
         public IActionResult CreateExcel(Guid guid)
         {
             if (guid == Guid.Empty)
