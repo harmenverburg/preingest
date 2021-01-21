01deb1daa92826cee5684a8862c73d64f52e114b
nhapreingest <73949448+nhapreingest@users.noreply.github.com>
2021-01-21 17:15:07 +0100

    Distinction between pdf/droid/planets in reporthandler

---
+++ droid-validation/src/main/java/nl/noord/hollandsarchief/droid/handlers/ReportingPlanetsXmlHandler.java
@@ -0,0 +1,46 @@
+package nl.noord.hollandsarchief.droid.handlers;
+
+import java.io.IOException;
+
+import nl.noord.hollandsarchief.droid.entities.BodyAction;
+import nl.noord.hollandsarchief.droid.entities.NewActionResult;
+
+public class ReportingPlanetsXmlHandler extends CommandHandler {
+    private String _guid = null;
+
+    public ReportingPlanetsXmlHandler(String guid) {
+        this._guid = guid;
+    }
+
+    public NewActionResult execute() {
+        NewActionResult result = null;
+        String[] command = { "java", "-jar",
+                String.format("%1$2sdroid-command-line-6.5.jar", new Object[] { this.DROID_LINUX_FOLDER }), "-r",
+                // String.format("%1$2s%2$2s/%2$2s.planets.xml", new Object[] {
+                // this.ARCHIVEDATA_LINUX_FOLDER, this._guid }),
+                String.format("%1$2s%2$2s/%3$2s.planets.xml",
+                        new Object[] { this.ARCHIVEDATA_LINUX_FOLDER, this._guid, "DroidValidationHandler" }),
+                "-t", "\"Planets XML\"", "-n", "\"Comprehensive breakdown\"", "-p",
+                // String.format("%1$2s%2$2s/%2$2s.droid", new Object[] {
+                // this.ARCHIVEDATA_LINUX_FOLDER, this._guid })
+                String.format("%1$2s%2$2s/%3$2s.droid",
+                        new Object[] { this.ARCHIVEDATA_LINUX_FOLDER, this._guid, "DroidValidationHandler" }) };
+        try {
+            if (command.length > 0) {
+                BodyAction jsonData = new BodyAction();
+                jsonData.name = ReportingDroidXmlHandler.class.getSimpleName() + " - Planets XML report";
+                jsonData.description = String.join(" ", command);
+                jsonData.result = "DroidValidationHandler.planets.xml";
+
+                result = this.registerNewAction(this._guid, jsonData);
+                String processId = result != null ? result.processId : null;
+                runSeperateThread(ReportingDroidXmlHandler.class.getSimpleName(), processId, this._guid, command);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
