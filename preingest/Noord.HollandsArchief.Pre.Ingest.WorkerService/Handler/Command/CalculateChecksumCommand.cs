7ea0ce45317eec59b66de060c561964d30303f36
nhapreingest <73949448+nhapreingest@users.noreply.github.com>
2021-02-01 07:28:28 +0100

    Fix #74
    Call start and fail status in this situation and throw an exception.
    Workerservice logs the output to console/container.

--- preingest/Noord.HollandsArchief.Pre.Ingest.WorkerService/Handler/Command/CalculateChecksumCommand.cs
+++ preingest/Noord.HollandsArchief.Pre.Ingest.WorkerService/Handler/Command/CalculateChecksumCommand.cs
@@ -14,11 +14,24 @@ namespace Noord.HollandsArchief.Pre.Ingest.WorkerService.Handler.Command
            {
                Logger.LogInformation("Command: {0} .", this.GetType().Name);
                OpenAPIService.Client api = new OpenAPIService.Client(WebApi.ToString(), client);
-               api.ApiPreingestCalculateAsync(this.CurrentSessionId, new OpenAPIService.BodyChecksum
+
+               if (CurrentSettings == null)
                {
-                   ChecksumType = CurrentSettings.ChecksumType,
-                   InputChecksumValue = CurrentSettings.ChecksumValue
-               }).GetAwaiter().GetResult();
+                   //save a start record
+                   api.ApiStatusStartAsync(this.CurrentSessionId).GetAwaiter().GetResult();                   
+                   string message = "Settings is empty! Object reference is null. Please save the settings first before starting the run.";
+                   //save a failed record
+                   api.ApiStatusFailedAsync(this.CurrentSessionId, new OpenAPIService.BodyMessage { Message = message }).GetAwaiter().GetResult();
+                   throw new ApplicationException(message);
+               }
+               else
+               {
+                   api.ApiPreingestCalculateAsync(this.CurrentSessionId, new OpenAPIService.BodyChecksum
+                   {
+                       ChecksumType = CurrentSettings.ChecksumType,
+                       InputChecksumValue = CurrentSettings.ChecksumValue
+                   }).GetAwaiter().GetResult();
+               }
            });
         }
     }
