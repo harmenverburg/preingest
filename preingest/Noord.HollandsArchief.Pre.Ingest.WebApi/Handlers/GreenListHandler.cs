7fdbf12f99b1f55ec19f73aad6ee31369c204a69
Wai Wong <73949448+nhapreingest@users.noreply.github.com>
2021-03-08 10:15:26 +0100

    GreenlistHandler condition update requested in #92

--- preingest/Noord.HollandsArchief.Pre.Ingest.WebApi/Handlers/GreenListHandler.cs
+++ preingest/Noord.HollandsArchief.Pre.Ingest.WebApi/Handlers/GreenListHandler.cs
@@ -76,9 +76,10 @@ namespace Noord.HollandsArchief.Pre.Ingest.WebApi.Handlers
                     throw new FileNotFoundException("Greenlist JSON file not found!", greenListLocation);
 
                 string extensionJson = File.ReadAllText(greenListLocation);
-                var extensionData = JsonConvert.DeserializeObject<GreenListItem[]>(extensionJson);
+                var extensionData = JsonConvert.DeserializeObject<GreenListItem[]>(extensionJson).ToList();
 
                 OnTrigger(new PreingestEventArgs { Description = "Read CSV file.",  Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
+
                 using (var reader = new StreamReader(droidCsvFile))
                 {
                     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -86,43 +87,73 @@ namespace Noord.HollandsArchief.Pre.Ingest.WebApi.Handlers
                         var records = csv.GetRecords<dynamic>().ToList();
 
                         var filesByDroid = records.Where(item
-                            => item.TYPE == "File" && item.EXT != "metadata").Select(item => new
+                            => item.TYPE == "File" && item.EXT != "metadata").Select(item => new DataItem
                             {
-                                SessionId = SessionGuid,
                                 Location = item.FILE_PATH,
                                 Name = item.NAME,
                                 Extension = item.EXT,
                                 FormatName = item.FORMAT_NAME,
                                 FormatVersion = item.FORMAT_VERSION,
-                                Puid = item.PUID
+                                Puid = item.PUID,
+                                IsExtensionMisMatch = item.EXTENSION_MISMATCH
                             }).ToList();
-
+                                                
+                        var actionDataList = new List<DataItem>();
                         eventModel.Summary.Processed = filesByDroid.Count;
+                        eventModel.Summary.Accepted = 0;
+                        eventModel.Summary.Rejected = 0;
+                        filesByDroid.ForEach(file =>                        
+                        {
+                            OnTrigger(new PreingestEventArgs { Description = string.Format( "Processing {0}", file.Location), Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
 
-                        OnTrigger(new PreingestEventArgs { Description = "Filter only files (no folders or files with .metadata extension).", Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
+                            //no puid found
+                            if (string.IsNullOrEmpty(file.Puid))
+                            {
+                                actionDataList.Add(new DataItem { Puid = file.Puid, Name = file.Name, Location = file.Location, FormatVersion = file.FormatVersion, FormatName = file.FormatName, Extension = file.Extension, InGreenList = false, Message = "Geen Pronom ID gevonden." });
+                                eventModel.Summary.Rejected = eventModel.Summary.Rejected + 1;
+                                return;
+                            }
+                            
+                            //extension mismatch
+                            bool parseOutput = false;
+                            Boolean.TryParse(file.IsExtensionMisMatch, out parseOutput);                            
+                            if (parseOutput)
+                            {
+                                actionDataList.Add(new DataItem { Puid = file.Puid, Name = file.Name, Location = file.Location, FormatVersion = file.FormatVersion, FormatName = file.FormatName, Extension = file.Extension, InGreenList = false, Message = "Verkeerde extensie combinatie gevonden." });
+                                eventModel.Summary.Rejected = eventModel.Summary.Rejected + 1;
+                                return;
+                            }
 
-                        var doesContainsGreenListResult = filesByDroid.Where(item
-                            => extensionData.Select(green
-                                => green.Extension.ToUpper()).ToList().Contains(item.Extension.ToUpper())).ToList();
+                            //pronom in nha list
+                            bool existsPuidInNhaList = extensionData.Exists(item => item.Puid.Equals(file.Puid, StringComparison.InvariantCultureIgnoreCase));
+                            if (existsPuidInNhaList)
+                            {
+                                file.InGreenList = true;
+                                file.Message = "Pronom ID gevonden in NHA groene lijst.";
+                                actionDataList.Add(file);
+                                eventModel.Summary.Accepted = eventModel.Summary.Accepted + 1;
+                                return;
+                            }
+
+                            //extension in nha list 
+                            bool existsExtInNhaList = extensionData.Exists(item => item.Extension.Equals(file.Extension, StringComparison.InvariantCultureIgnoreCase));
+                            if (!existsExtInNhaList)
+                            {
+                                actionDataList.Add(new DataItem { Puid = file.Puid, Name = file.Name, Location = file.Location, FormatVersion = file.FormatVersion, FormatName = file.FormatName, Extension = file.Extension, InGreenList = false, Message = "Extensie en/of Pronom ID niet gevonden in NHA groene lijst." });
+                                eventModel.Summary.Rejected = eventModel.Summary.Rejected + 1;
+                            }                               
+                            else
+                            {
+                                file.InGreenList = true;
+                                file.Message = String.Format ("Extensie gevonden in NHA groene lijst maar wel met een andere Pronom ID. Droid Pronom = {0}, NHA groene lijst = {1}.", file.Puid, extensionData.FirstOrDefault(item => item.Extension.Equals(file.Extension, StringComparison.InvariantCultureIgnoreCase)).Puid);
+                                actionDataList.Add(file);
+                                eventModel.Summary.Accepted = eventModel.Summary.Accepted + 1;
+                            }
+                        }); 
                         
-                        OnTrigger(new PreingestEventArgs { Description = "Filter the content.", Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
-
-                        var doNotcontainsGreenListResult = filesByDroid.Except(doesContainsGreenListResult).ToList();
-
-                        OnTrigger(new PreingestEventArgs { Description = "Compare both lists.", Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
-
-                        var endResult = doesContainsGreenListResult.Select(item
-                            => new { item.SessionId, item.Puid, item.Name, item.Location, item.FormatVersion, item.FormatName, item.Extension, InGreenList = true })
-                            .Concat(doNotcontainsGreenListResult.Select(item
-                            => new { item.SessionId, item.Puid, item.Name, item.Location, item.FormatVersion, item.FormatName, item.Extension, InGreenList = false }))
-                            .ToList();
+                        eventModel.ActionData = actionDataList.ToArray(); 
 
                         OnTrigger(new PreingestEventArgs { Description = "Done comparing both lists.", Initiate = DateTimeOffset.Now, ActionType = PreingestActionStates.Executing, PreingestAction = eventModel });
-                        
-                        eventModel.Summary.Accepted = doesContainsGreenListResult.Count();
-                        eventModel.Summary.Rejected = doNotcontainsGreenListResult.Count();
-                        
-                        eventModel.ActionData = endResult.ToArray();
                     }
                 }
                 isSuccess = true;
@@ -159,6 +190,19 @@ namespace Noord.HollandsArchief.Pre.Ingest.WebApi.Handlers
         public void Dispose()
         {
             this.PreingestEvents -= Trigger;
+        }
+
+        internal class DataItem
+        {
+            public string Location { get; set; }
+            public string Name { get; set; }
+            public string Extension { get; set; }
+            public string FormatName { get; set; }
+            public string FormatVersion { get; set; }
+            public string Puid { get; set; }
+            public string IsExtensionMismatch { get; set; }
+            public string Message { get; set; }
+            public bool InGreenList { get; set; }
         }
     }
 }
